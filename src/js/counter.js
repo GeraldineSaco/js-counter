2524bd258063dd2acb4ef29d8361d6772b225c76
GeraldineSaco <gsacoruiz@gmail.com>
2026-01-05 01:52:45 +0100

    functions completed

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,23 @@
-function add(){
+function add(){    
     //Escribir tu código que aumenta
+    const nroCount = document.getElementById("number");
+    let currentNumber = parseInt(nroCount.textContent);
+    nroCount.textContent = currentNumber + 1;
 }
 
 function reset(){
     //Escribir tu código que hace un reset a 0
+    const nroCount = document.getElementById("number");
+    nroCount.textContent = 0;
 }
 
 function decrease(){
     //Escribir tu código que resta
+    const nroCount = document.getElementById("number");
+    let currentNumber = parseInt(nroCount.textContent);
+    if (nroCount.textContent > 0){
+        nroCount.textContent = currentNumber - 1;
+    }
 };
 
 export {add, reset, decrease}
--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,10 @@
 import {add, reset, decrease} from './counter.js'
 
-
 //Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+const addBtn = document.getElementById("add");
+
+decreaseBtn.addEventListener("click", decrease);
+resetBtn.addEventListener("click", reset);
+addBtn.addEventListener("click", add);
